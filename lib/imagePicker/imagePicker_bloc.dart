ab16f6c14be999c46a0bf5d41328e3920f309f28
ANAS MUFTI <muftianas97@gmail.com>
2025-02-14 18:45:15 +0500

    .

--- lib/imagePicker/imagePicker_bloc.dart
+++ lib/imagePicker/imagePicker_bloc.dart
@@ -4,34 +4,26 @@ import 'package:block_pattern_flutter_app/utils/utils.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:image_picker/image_picker.dart';
 
-class ImagePickerBloc extends Bloc<ImagePickerEvent, ImagePickerState>{
-
-  final ImagePickerUtils imagePickerUtils ;
+class ImagePickerBloc extends Bloc<ImagePickerEvent, ImagePickerState> {
+  final ImagePickerUtils imagePickerUtils;
 
   ImagePickerBloc(this.imagePickerUtils) : super(ImagePickerState()) {
-
-                on<CameraCapture>(_cameraCapture);
+    on<CameraCapture>(_cameraCapture);
 
     on<GalleryPicker>(_galleryPicker);
-
   }
 
-  void _cameraCapture(CameraCapture event , Emitter<ImagePickerState> emit)async{
-
+  void _cameraCapture(
+      CameraCapture event, Emitter<ImagePickerState> emit) async {
     XFile? file = await imagePickerUtils.cameraCapture();
 
     emit(state.copyWith(file: file));
-
   }
 
-
-
-  void _galleryPicker(GalleryPicker event , Emitter<ImagePickerState> emit)async{
-
+  void _galleryPicker(
+      GalleryPicker event, Emitter<ImagePickerState> emit) async {
     XFile? file = await imagePickerUtils.pickImageFromGallery();
 
     emit(state.copyWith(file: file));
   }
-
-
 }
--- lib/imagePicker/imagePicker_event.dart
+++ lib/imagePicker/imagePicker_event.dart
@@ -1,24 +1,12 @@
 import 'package:equatable/equatable.dart';
 
-abstract class ImagePickerEvent extends Equatable{
+abstract class ImagePickerEvent extends Equatable {
   const ImagePickerEvent();
 
   @override
   List<Object> get props => [];
 }
 
+class CameraCapture extends ImagePickerEvent {}
 
-class CameraCapture extends ImagePickerEvent {
-
-
-
-}
-class GalleryPicker extends ImagePickerEvent {
-
-
-
-
-
-}
-
-
+class GalleryPicker extends ImagePickerEvent {}
