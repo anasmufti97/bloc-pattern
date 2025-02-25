f8c0468d741196ae386be91c2dd800728bf939ef
ANAS MUFTI <muftianas97@gmail.com>
2025-02-25 17:21:11 +0500

    own Apis added in the app

---
+++ lib/apisHandling/api_services.dart
@@ -0,0 +1,141 @@
+// import 'package:flutter/foundation.dart';
+// import 'package:http/http.dart' as http;
+// import 'dart:convert';
+// import '../../Utils/utils.dart';
+//
+// class ApiServices {
+//   /// Post Api Service
+//   // static Future<String?> postMethod({required String url, Map<String, dynamic>? body}) async {
+//   //   try {
+//   //     final headers = {
+//   //       'Content-Type': 'application/json',
+//   //       if (authProvider.getToken() != null) 'Authorization': 'Bearer ${authProvider.getToken()}'
+//   //     };
+//   //     final request = http.Request('POST', Uri.parse(url));
+//   //     if (body != null) {
+//   //       request.body = json.encode(body);
+//   //     }
+//   //     request.headers.addAll(headers);
+//   //     http.StreamedResponse response = await request.send();
+//   //
+//   //     final result = await response.stream.bytesToString();
+//   //     if (response.statusCode == 401) {
+//   //       return null;
+//   //     }
+//   //
+//   //     if (response.statusCode == 200 || response.statusCode == 201) {
+//   //       return result;
+//   //     } else {
+//   //       try {
+//   //         final message = json.decode(result)['message'];
+//   //         return null;
+//   //       } on Exception catch (e) {
+//   //         return null;
+//   //       }
+//   //     }
+//   //   } on Exception catch (e) {
+//   //     return null;
+//   //   }
+//   // }
+//
+//   /// GET Api Service
+//   static Future<String?> getMethod(String url) async {
+//     try {
+//       final headers = {
+//         'Content-Type': 'application/json',
+//         if (authProvider.getToken() != null) 'Authorization': 'Bearer ${authProvider.getToken()}'
+//       };
+//       final request = http.Request('GET', Uri.parse(url));
+//       request.headers.addAll(headers);
+//       http.StreamedResponse response = await request.send();
+//
+//       final result = await response.stream.bytesToString();
+//       if (response.statusCode == 401) {
+//         // authProvider.logoutUser();
+//         return null;
+//       }
+//       if (response.statusCode == 200 || response.statusCode == 201) {
+//         return result;
+//       }
+//
+//       final jd=json.decode(result);
+//       return null;
+//     } on Exception catch (e) {
+//       return null;
+//     }
+//   }
+//
+//   /// Post multipart
+//
+//   static Future<String?> multipartPostMethod({
+//     required String url,
+//     required Map<String, String> fields,
+//   }) async {
+//     try {
+//       final headers = {
+//         'Content-Type': 'application/json',
+//         if (authProvider.getToken() != null) 'Authorization': 'Bearer ${authProvider.getToken()}'
+//       };
+//
+//       final request = http.MultipartRequest('POST', Uri.parse(url));
+//       request.fields.addAll(fields);
+//       request.headers.addAll(headers);
+//
+//       http.StreamedResponse response = await request.send();
+//
+//       if (response.statusCode == 200) {
+//         final result = await response.stream.bytesToString();
+//         return result;
+//       } else {
+//         final result = await response.stream.bytesToString();
+//         final message = json.decode(result)['message'];
+//         return null;
+//       }
+//     } on Exception catch (e) {
+//       // Handle exceptions
+//       if (kDebugMode) print(e.toString());
+//       return null;
+//     }
+//   }
+// }
+
+
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:block_pattern_flutter_app/apisHandling/model.dart';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+
+  static final String _baseUrl = "https://413c-182-185-158-96.ngrok-free.app";
+
+  static Future<void> addNote(Note note) async {
+    Uri requestUri = Uri.parse("$_baseUrl/addNote");
+    var response = await http.post(requestUri, body: note.toMap());
+    var decoded = jsonDecode(response.body);
+    log(decoded.toString());
+  }
+
+  static Future<void> deleteNote(Note note) async {
+    Uri requestUri = Uri.parse("$_baseUrl/delete");
+    var response = await http.post(requestUri, body: note.toMap());
+    var decoded = jsonDecode(response.body);
+    log(decoded.toString());
+  }
+
+  static Future<List<Note>> fetchNotes() async {
+    Uri requestUri = Uri.parse("$_baseUrl/read");
+    var response = await http.get(requestUri);
+    var decoded = jsonDecode(response.body);
+
+    List<Note> notes = [];
+    for(var noteMap in decoded) {
+      Note newNote = Note.fromMap(noteMap);
+      notes.add(newNote);
+    }
+
+    return notes;
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:block_pattern_flutter_app/ToDoList/to_do_list_bloc.dart';
 import 'package:block_pattern_flutter_app/ToDoList/to_do_list_screen.dart';
+import 'package:block_pattern_flutter_app/apisHandling/noteProvider.dart';
+import 'package:block_pattern_flutter_app/apisHandling/view/homePage.dart';
 import 'package:block_pattern_flutter_app/imagePicker/imagePicker_bloc.dart';
 import 'package:block_pattern_flutter_app/imagePicker/imagePicker_screen.dart';
 import 'package:block_pattern_flutter_app/switch/switch_bloc.dart';
@@ -7,6 +9,7 @@ import 'package:block_pattern_flutter_app/switch/switch_screen.dart';
 import 'package:block_pattern_flutter_app/utils/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
 import 'counter/counter_bloc.dart';
 
 void main() {
@@ -16,21 +19,28 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
+    return MultiProvider(
       providers: [
-        BlocProvider<ImagePickerBloc>(
-            create: (context) => ImagePickerBloc(ImagePickerUtils())),
-        BlocProvider<SwitchBlock>(create: (context) => SwitchBlock()),
-        BlocProvider<ToDoBloc>(create: (context) => ToDoBloc()),
-        BlocProvider<CounterBloc>(create: (context) => CounterBloc()),
-        // BlocProvider<CounterBloc>(create: (context) => CounterBloc()), // Adding CounterBloc
+        ChangeNotifierProvider(
+          create: (context) => NotesProvider(),
+        ),
       ],
+      // MultiBlocProvider(
+      // providers: [
+      //   BlocProvider<ImagePickerBloc>(
+      //       create: (context) => ImagePickerBloc(ImagePickerUtils())),
+      //   BlocProvider<SwitchBlock>(create: (context) => SwitchBlock()),
+      //   BlocProvider<ToDoBloc>(create: (context) => ToDoBloc()),
+      //   BlocProvider<CounterBloc>(create: (context) => CounterBloc()),
+      //   BlocProvider<CounterBloc>(create: (context) => CounterBloc()), // Adding CounterBloc
+      // ],
 
 
 
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: ToDoScreen(),
+        home: HomePage(),
+        // home: ToDoScreen(),
         // home: SwitchWidget(),
         // home: CounterScreen(),
       ),
